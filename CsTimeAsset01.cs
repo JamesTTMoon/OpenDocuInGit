b05a865f9fd40c78e8beafdd533e09677fc815f1
Mooon <jongwoomooon@gmail.com>
2013-04-25 13:53:54 +0900

    Init

---
+++ CsTimeAsset01.cs
@@ -0,0 +1,16 @@
+
+public class MainWeek01 {
+
+    void D0425() {  
+        /* Target 100'
+         * 1:00 ~ 
+         * ~ 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * */
+    }
+
+}
---
+++ MdFormatter.py
@@ -0,0 +1,18 @@
+
+# create the root window
+
+class MdIntObj: # : MdObject {  // 123 ...
+    # 숫자를 정의하는 객체
+    
+    
+class MdFormatter: # : MdObject
+    # 애니메이션 스텝, 좌표, 시간 등 프로세스를 기록하여
+    # public List<MdFormatter> arrFormat;  에 보관..
+    
+    def MdFormatter(int pForm, int pLatt, Godirum pGod)
+        # 12 + 34 같은 경우를 정의하는 함수. 
+        
+        
+        
+
+    
