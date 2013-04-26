ef9002ce10b5f1f92a113a15897672c9a9c3de20
Mooon <jongwoomooon@gmail.com>
2013-04-26 11:56:41 +0900

    JJ

--- CsTimeAsset01.py
+++ CsTimeAsset01.py
@@ -1,8 +1,11 @@
 
 class MainWeek01 :
-    
-
-    def D0425() :  # Target 100'
+    def D0425(Thursday) :  # Target 100'
         A1 = "1:00 ~ 50 :   50  :   Base Setting. Just run it.   "
         A2 = "2:30 ~ 00 :   30  :   Jinsim Intro "
         A3 = "3:30 ~ 50 :   20  :   Jinsim Position "
+
+    def D0426(Friday) :  # Target 150'
+        A1 = "9:50 ~ 40 :   50  :   JJ, Hon 초기 작업." 
+        A2 = "11:10~ 00 :   50  :   Set Target, Jumbi, Chum (main transformation) "
+        A2 = "   :10~ 00 :   50  :   Jumbi,  Chum Anima  ..."
---
+++ JJ.py
@@ -0,0 +1,31 @@
+
+# JJ :  전역 자료형.
+
+class JJ: # : 전역 변수.. 자료형 ..
+    
+    # public static List<List<VecRot>> arrJinsimAe = new List<List<VecRot>>();
+    arrJinsimAe # 리스트의 리스트.. 3 은 arrJinsimAe[3]  또는 arrJinsimAe[(int)myFff] 로 가져오면 VecRot 의 어레이..
+    
+    
+    def Init(): # static ...
+        pass # 이 함수를 실행 시켜야 '진심' 위치를 세팅함.
+        
+    def MdIntObj (): # (int pValue, bool pIsTotal): // 10 , 5 , ...
+        #// MdIntObj intObj = new MdIntObj (1045, true);  1000, 0, 40, 5 ...
+        # 토탈의 경우 ... 각 자릿수의 숫자로 나눠 보관.
+        #//("MdIntObj :: MdIntObj   " + pValue + " ,  Total ? "
+        
+        pass
+
+class MdFormatter: # : MdObject
+    # 애니메이션 스텝, 좌표, 시간 등 프로세스를 기록하여
+    # public List<MdFormatter> arrFormat;  에 보관..
+    
+    def MdFormatter() : #(int pForm, int pLatt, Godirum pGod):
+        # 12 + 34 같은 경우를 정의하는 함수. 
+        
+        pass
+        
+        
+
+    
